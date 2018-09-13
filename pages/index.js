273deb7034acf3611c11501c21c19170a3323c3f
kimdugong <js0628@ymail.com>
2018-09-13 13:46:40 +0900

    Initializing history of transactions

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,46 @@ import ripple from '../ripple';
 import TxRow from '../components/TxRow';
 import { Container, Table } from 'semantic-ui-react';
 import Head from 'next/head';
+import axios from 'axios';
+
+const api = 'https://data.ripple.com/v2';
+const myAddress = 'rN3dBbAqB7b47GB2BPKvP3Y61pkciNKiE5';
 
 class RippleWatch extends Component {
   static async getInitialProps() {
     await ripple.connect();
-    const address = await ripple.getAccountInfo(
-      'rN3dBbAqB7b47GB2BPKvP3Y61pkciNKiE5'
-    );
+    const address = await ripple.getAccountInfo(myAddress);
     const ledger = await ripple.getLedgerVersion();
-    console.log(address);
-    return { address, ledger };
+    const transactions = await ripple.getTransactions(myAddress, {
+      minLedgerVersion: 12500000,
+      maxLedgerVersion: ledger
+    });
+    return { address, ledger, transactions };
   }
 
   renderRow = txs =>
     txs.map(tx => <TxRow transaction={tx} key={tx.transaction.hash} />);
 
   componentDidMount = async () => {
+    const transactions = this.props.transactions.map(tx => {
+      const transaction = {
+        transaction: {
+          hash: tx.id,
+          Destination: tx.specification.destination.address,
+          Account: tx.address,
+          Amount: tx.specification.destination.amount.value * 10 ** 6
+        }
+      };
+      return transaction;
+    });
+    this.setState(prev => ({ txs: transactions }));
     await ripple.connect();
     ripple.on('ledger', ledger => this.setState({ currentLedger: ledger }));
     ripple.connection.on('transaction', tx => {
+      console.log('tx listening  : ', tx);
       const affectedNodes = tx.meta.AffectedNodes;
       const myNode = affectedNodes.filter(
-        node =>
-          node.ModifiedNode.FinalFields.Account ===
-          'rN3dBbAqB7b47GB2BPKvP3Y61pkciNKiE5'
+        node => node.ModifiedNode.FinalFields.Account === myAddress
       );
 
       this.setState(prev => ({
@@ -37,7 +53,7 @@ class RippleWatch extends Component {
     });
 
     ripple.request('subscribe', {
-      accounts: ['rN3dBbAqB7b47GB2BPKvP3Y61pkciNKiE5']
+      accounts: [myAddress]
     });
   };
 
@@ -75,7 +91,7 @@ class RippleWatch extends Component {
           </Header>
           <Body>
             <Row>
-              <Cell>{'rN3dBbAqB7b47GB2BPKvP3Y61pkciNKiE5'}</Cell>
+              <Cell>{myAddress}</Cell>
               <Cell>{this.state.xrpBalance}</Cell>
               <Cell>
                 {this.state.previousAffectingTransactionLedgerVersion}
